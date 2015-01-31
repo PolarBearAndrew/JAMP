9d727e3d61340c0d036e7810984dcdb15e56c67b
Andrew <chenpoanandrew@gmail.com>
2015-02-01 00:00:48 +0800

    一對一聊天ok

--- app_chat.js
+++ app_chat.js
@@ -9,24 +9,28 @@ app.get('/chatroom.html', function (req, res) {
 
 io.on('connection', function (socket) { //連線成功時
 	socket.on('msg', function (data) { //掛載msg事件-->代表有人說話
-		
-		console.log(data.to);
-		console.log(data.say);
-		
-		if (data.to == 'all') { //給所有人
-			socket.broadcast.emit('say', data);
-		} else { //指定某群人
-//			var clients = io.sockets.clients(); //client名單
-//			var nameList = data.to.splice(','); //取得要送給誰的名單
-//			//逐一檢查是否對該client訊息
-//			clients.forEach(function (client) {
-//				for (var i = nameList.length - 1; i >= 0; i--) { //逐一檢查
-//					if (client.name == nameList[i]) {
-//						client.emit('say', data); //發訊息給他
-//					}
-//				}
-//			});
-		}
+
+			socket.emit('say', data);
+
+			//if (data.to == 'all') { //給所有人
+			//			console.log(data);
+			//	socket.emit('say', data);
+			//} else { //指定某群人
+			//			var clients = io.sockets.clients(); //client名單
+			//			var nameList = data.to.splice(','); //取得要送給誰的名單
+			//			
+			//			
+			//			console.log(nameList);
+			//			
+			//			//逐一檢查是否對該client訊息
+			//			clients.forEach(function (client) {
+			//				for (var i = nameList.length - 1; i >= 0; i--) { //逐一檢查
+			//					if (client.name == nameList[i]) {
+			//						client.emit('say', data); //發訊息給他
+			//					}
+			//				}
+			//			});
+//		}
 	});
 });
 
