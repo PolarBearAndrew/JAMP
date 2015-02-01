0b3e2990a0907c0089bf34d502222fd4e779cdaa
rocktsuki <rocktsuki@gmail.com>
2015-02-01 17:44:36 +0800

    req.cookie modify version

--- app.js
+++ app.js
@@ -1,101 +1,96 @@
 var path = require('path'),
-	http = require('http'),
-	express = require('express'),
-	bodyParser = require('body-parser'),
-	cookieParser = require('cookie-parser'),
-	app = express(),
-	server = app.listen(3000, function () {
-		console.log('listen on *:3000');
-	});
-io = require('socket.io')(server);
+    http = require('http'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    app = express(),
+    server = app.listen(3000, function() {
+        console.log('listen on *:3000');
+    }),
+    io = require('socket.io')(server);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
-	extended: false
+    extended: false
 }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use('/public', express.static(__dirname + '/public'));
 
-
-
-
 var users = {};
 
-app.get('/', function (req, res) {
-	if (req.cookies.user == null) {
-		res.redirect('/signin');
-	} else {
-		res.render('chat.ejs');
-	}
+app.get('/', function(req, res) {
+    if (req.cookies.user == null) {
+        res.redirect('/signin');
+    } else {
+        res.render('chatroom');
+    }
 });
 
-app.get('/signin', function (req, res) {
-	res.render('login.ejs');
+// 登入頁
+app.get('/signin', function(req, res) {
+    res.render('login');
 });
 
-app.post('/formLogin', function (req, res) {
-	if (users[req.body.name]) {
-		res.redirect('/signin'); //
-	} else {
-		//≤ª¥Ê‘⁄£¨É¶¥Ê”√ëÙ cookie ÅKÃ¯ﬁD÷˜Ìì
-		res.cookie("user", req.body.name, {
-			maxAge: 1000 * 60
-		});
-
-
-		users[req.body.name] = {
-			name: req.body.name,
-			password: req.body.password,
-			pos: req.body.pos,
-			pro: req.body.pro
-		};
-
-		res.sendFile(__dirname + '/chatroom.html');
-	}
+// 登入
+app.post('/formLogin', function(req, res) {
+    if (users[req.body.myName]) {
+        // 會員名稱重複則重新登入
+        res.redirect('/signin'); //
+    } else {
+        // 登入成功將會員資料setToCookie
+        var userObj = {
+            myName: req.body.myName,
+            password: req.body.password,
+            pos: req.body.pos,
+            pro: req.body.pro
+        };
+        res.cookie("user", JSON.stringify(userObj), {
+            maxAge: 1000 * 60 * 30
+        });
+        res.redirect('/');
+    }
 });
 
-app.post('/formLogout', function (req, res) {
-	res.render('login.ejs');
+// 登出
+app.post('/formLogout', function(req, res) {
+    res.clearCookie('user'); // 清除cookie
+    res.redirect("/");
 });
 
+// 連線
+io.sockets.on('connection', function(socket) {
+    // server notice that somebody does login with all
+    socket.on('online', function(data) {
+        socket.myName = data.myName;
+        if (!users[data.myName]) {
+            users[data.myName] = data;
+        };
+        var tmp = users;
+        console.log(tmp);
+        io.sockets.emit('online', { user: data, users: tmp });
+    });
 
-io.sockets.on('connection', function (socket) {
-	// server notice that somebody does login with all
-	socket.on('online', function (data) {
+    // server notice that somebody does loginout with all
+    socket.on('disconnect', function() {
 
-		//		socket.name = data.user;
-		//		if (!users[data.user]) {
-		//			users[data.user] = data.user;
-		//		}
+        if (users[socket.myName]) {
 
-		//		console.log("user" + data.user + "\n----------");
-		//console.log("users" + users + "\n----------");
-		io.sockets.emit('online', {
-			users: users,
-			user: data
-		});
-	});
+            delete users[socket.myName];
 
-	// server notice that somebody does loginout with all
-	socket.on('disconnect', function () {
-		//»Ù users ∂‘œÛ÷–±£¥Ê¡À∏√”√ªß√˚
-		if (users[socket.name]) {
-			//¥” users ∂‘œÛ÷–…æ≥˝∏√”√ªß√˚
-			delete users[socket.name];
-			//œÚ∆‰À˚À˘”–”√ªßπ„≤•∏√”√ªßœ¬œﬂ–≈œ¢
-			socket.broadcast.emit('offline', {
-				users: users,
-				user: socket.name
-			});
-		}
-	});
+            socket.broadcast.emit('offline', {
+                users: users,
+                user: socket.myName
+            });
+        }
+    });
 
-	socket.on('msg', function (data) { //掛載msg事件-->代表有人說話
-		socket.broadcast.emit('say', data);
-		socket.emit('say', data);
-	});
+    socket.on('msg', function(data) { //掛載msg事件-->代表有人說話
+        // socket.broadcast.emit('say', data);
+        // console.log(data);
+        io.sockets.emit('say', data);
+    });
 
 });
